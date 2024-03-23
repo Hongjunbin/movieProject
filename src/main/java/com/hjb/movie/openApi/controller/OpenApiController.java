c888563fc501312496380db92ad03b1f761a2d3a
Sunj33 <bsj1180@gmail.com>
2024-03-23 18:54:57 +0900

    HttpClient 라이브러리 추가

--- src/main/java/com/hjb/movie/openApi/controller/OpenApiController.java
+++ src/main/java/com/hjb/movie/openApi/controller/OpenApiController.java
@@ -1,7 +1,28 @@
 package com.hjb.movie.openApi.controller;
 
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.IOException;
 @Controller
 public class OpenApiController {
+
+    @GetMapping( "/getMovieInfo" )
+    public void getMovieInfoList() throws IOException {
+
+        HttpClient hc = new HttpClient();
+
+        GetMethod getMethod = new GetMethod("https://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json");
+        getMethod.setQueryString("key=e08881edc3158fd8194585483c48e21e");
+
+        hc.executeMethod( getMethod );
+
+        String responseBodyAsString = getMethod.getResponseBodyAsString();
+
+        System.out.println(responseBodyAsString);
+
+    }
+
 }
