70bc7d3478686217574a5451e0bf99f2b8fc04ec
Hongjunbin <ghdwnsqls0@naver.com>
2024-03-23 17:37:17 +0900

    good

--- src/main/java/com/hjb/movie/MovieApplication.java
+++ src/main/java/com/hjb/movie/MovieApplication.java
@@ -15,25 +15,25 @@ public class MovieApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MovieApplication.class, args);
 		
-		RestTemplate restTemplate = new RestTemplate();
-		String apiUrl = "https://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=e08881edc3158fd8194585483c48e21e";
-		String response = restTemplate.getForObject(apiUrl, String.class);
-		System.out.println(response);
-		
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode rootNode = objectMapper.readTree(response);
-
-            // JsonNode에서 필요한 값을 추출
-            String movieNm = rootNode.path("movieListResult").path("movieNm").asText(); // key1에 해당하는 값을 추출하여 문자열로 변환
-            String nationAlt = rootNode.path("nationAlt").asText(); // key2에 해당하는 값을 추출하여 정수로 변환
-
-            // 추출한 값들을 출력
-            System.out.println(movieNm);
-            System.out.println(nationAlt);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//		RestTemplate restTemplate = new RestTemplate();
+//		String apiUrl = "https://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=e08881edc3158fd8194585483c48e21e";
+//		String response = restTemplate.getForObject(apiUrl, String.class);
+//		System.out.println(response);
+//		
+//        try {
+//            ObjectMapper objectMapper = new ObjectMapper();
+//            JsonNode rootNode = objectMapper.readTree(response);
+//
+//            // JsonNode에서 필요한 값을 추출
+//            String movieNm = rootNode.path("movieListResult").path("movieNm").asText(); // key1에 해당하는 값을 추출하여 문자열로 변환
+//            String nationAlt = rootNode.path("nationAlt").asText(); // key2에 해당하는 값을 추출하여 정수로 변환
+//
+//            // 추출한 값들을 출력
+//            System.out.println(movieNm);
+//            System.out.println(nationAlt);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 		
 		
 	}
